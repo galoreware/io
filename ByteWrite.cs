bffc4c6b6d3e29da792d8d5289b5636cc24618a4
Gabriel Mtz Carrillo <gabrielmtzcarrillo@users.noreply.github.com>
2021-03-27 20:30:38 -0600

    Update ByteWrite.cs
    Added Read Funcionts

--- ByteWrite.cs
+++ ByteWrite.cs
@@ -11,6 +11,55 @@ namespace GaloreWare.IO
     {
         MemoryStream _data;
 
+        public long Offset(long index)
+        {
+            long r = 0;
+            long offset = index % _data.Length;
+
+            r = offset < 0 ? _data.Length - offset : offset;
+
+            return r;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                _data.Position = Offset(index);
+                return _data.ReadByte();
+            }
+
+            set
+            {
+                _data.Position = Offset(index);
+                _data.WriteByte((byte)value);
+            }
+        }
+
+        public byte[] this[int index, int length]
+        {
+            get
+            {
+                long position = Offset(index);
+
+                byte[] r = new byte[length];
+
+                _data.Position = position;
+                _data.Read(r, 0, length);
+
+                return r;
+            }
+        }
+
+        public ByteWrite(string filename)
+        {
+            _data = new MemoryStream();
+            using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+            {
+                fs.CopyTo(_data);
+            }
+        }
+
         public ByteWrite(int init_size=-1)
         {
             _data = init_size == -1 ? new MemoryStream() : new MemoryStream(init_size);
@@ -22,6 +71,21 @@ namespace GaloreWare.IO
             _data.Write(data, 0, size);
         }
 
+        public string ReadASCIIString(int offset, int length)
+        {
+            return ASCIIEncoding.ASCII.GetString(this[offset, length]).Replace("\0", string.Empty);
+        }
+
+        public Int16 ReadInt16(int offset)
+        {
+            return BitConverter.ToInt16(this[offset, 2], 0);
+        }
+
+        public int ReadInt32(int offset)
+        {
+            return BitConverter.ToInt32(this[offset, 4], 0);
+        }
+        
         public void WriteASCII(int offset, string str, int size)
         {
             byte[] buffer = ASCIIEncoding.ASCII.GetBytes(str);
@@ -38,6 +102,12 @@ namespace GaloreWare.IO
         {
             byte[] buffer = BitConverter.GetBytes(value);
             Write(offset, buffer, 4);
+        }
+        
+        public void Fill(int offset, int size)
+        {
+            int s = (int)Math.Max(_data.Length,offset+size);
+            _data.SetLength(s);
         }
 
         public void Save(string filename)
